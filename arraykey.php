56d1c64d765ea2df13e7b00fd2b77fdc15c44a23
bock1221 <tzvibock@gmail.com>
2013-08-17 02:23:09 -0400

    arraykeyexists

---
+++ arraykey.php
@@ -0,0 +1,56 @@
+<?php
+  $index = array('cart'=>NULL,'at'=>'home');
+  print_r($index);
+if(array_key_exists('cart' , $index)) {
+  echo 'array key is set';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>  
